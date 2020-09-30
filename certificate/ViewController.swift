a6749fa20309a2aec4f902370c7f15a48201f9e5
kaimu <ok89.bado@gmail.com>
2020-09-30 22:32:41 +0900

    first commit

---
+++ certificate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  certificate
+//
+//  Created by kaimu on 2020/09/30.
+//  Copyright © 2020 landlnad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
